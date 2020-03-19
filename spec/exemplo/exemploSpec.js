b0e43aab42298746b6d819438884bd760bbbc32f
Ezequiel Fernandes <ezequiel.fernandes@sotran.com>
2020-03-19 09:34:05 -0300

    Arquivos do Projeto Local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,11 @@
+describe("Suite de teste de exemplo com NodeJS", function(){
+    it("deve demonstrar o uso do jasmine com o NodeJS", function(){
+        expect(true).toEqual(true);
+    });  
+});
+
+describe("Suite de teste de exemplo com NodeJS 2", function(){
+    it("deve demonstrar o uso do jasmine com o NodeJS 2", function(){
+        expect(1).toEqual(2);
+    });  
+});
